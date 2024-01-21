6a97d1fa8d2b754b18d7cd48b8d35329caf23afc
Pavan Khotkar <khotkarpavan3@gmail.com>
2024-01-21 09:15:20 -0800

    Add files via upload

---
+++ example.py
@@ -0,0 +1,29 @@
+print("""hello
+hello
+hello
+hello
+hello
+ """)
+#print("hello im a hero.\n" + "hello am i hero\n" + "big hero")
+
+#print("dog\n"*90)
+name = input(":-What is your name\n")
+print(":-hello " + name + " thank you for coming today\n")
+if name == "etc":
+    print("[your name is not correct Please write correct name]\n")
+else:
+    print("[you are guild member]\n")
+input("[how can i help you today]\n")
+menu = " [1]cocktail\n [2]soft drink\n [3]hard drink\n"
+print("menu name\n" + menu)
+
+name = input("which would you like  ")
+
+#print(name)
+
+print(name+ " wait for while")
+print("come get your coffee sir")
+#print("thanks")
+
+#name = "alpha"
+#print(name)
